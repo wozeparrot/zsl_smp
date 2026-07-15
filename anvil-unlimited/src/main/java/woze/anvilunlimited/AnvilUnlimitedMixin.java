b0d7362769a862ea1bb33c0bb96529e57c1069aa
Woze Parrot <wozeparrot@gmail.com>
2026-07-14 23:56:08 -0400

    fix: more fixes

--- anvil-unlimited/src/main/java/woze/anvilunlimited/AnvilUnlimitedMixin.java
+++ anvil-unlimited/src/main/java/woze/anvilunlimited/AnvilUnlimitedMixin.java
@@ -7,15 +7,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import woze.anvilcore.AnvilUnlimited;
 
-@Mixin(targets = "fuzs.easyanvils.world.inventory.ModAnvilMenu")
+@Mixin(targets = "fuzs.easyanvils.common.world.inventory.ModAnvilMenu")
 public class AnvilUnlimitedMixin {
 
-    @Inject(method = "createResult", at = @At("HEAD"))
+    @Inject(method = "createAnvilResult", at = @At("HEAD"))
     private void setFlag(CallbackInfo ci) {
         AnvilUnlimited.IN_ANVIL.set(true);
     }
 
-    @Inject(method = "createResult", at = @At("RETURN"))
+    @Inject(method = "createAnvilResult", at = @At("RETURN"))
     private void clearFlag(CallbackInfo ci) {
         AnvilUnlimited.IN_ANVIL.remove();
     }
